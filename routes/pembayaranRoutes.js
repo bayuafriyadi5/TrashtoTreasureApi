08c4caf4610ca7cb862a1035ebf422d7709170e5
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 00:40:23 +0700

    Update pembayaranRoutes.js

--- routes/pembayaranRoutes.js
+++ routes/pembayaranRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pembayaranController = require('../controllers/pembayaranController');
 
 router.get('/', pembayaranController.getAllPembayaran);
-router.get('/find', pembayaranController.findPembayaranByName);
 router.get('/:id_pembayaran', pembayaranController.getPembayaranById);
 
 module.exports = router;
