062b320c94bef6e9ef045825139ee3ebcdcfb944
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-28 14:02:02 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize({
-//     "username": "uyjhigauptnee1jk",
-//     "password": "KdcwlywLFEukDTa5V2DN",
-//     "database": "begnhhimrzdbwryxnjrr",
-//     "host": "begnhhimrzdbwryxnjrr-mysql.services.clever-cloud.com",
-//     "dialect": "mysql",
-//     "dialectModule": require("mysql2"),
-//     "benchmark": true
-// });
-
 const sequelize = new Sequelize({
-    "username": "root",
-    "password": "",
-    "database": "ttt",
-    "host": "localhost",
+    "username": "uyjhigauptnee1jk",
+    "password": "KdcwlywLFEukDTa5V2DN",
+    "database": "begnhhimrzdbwryxnjrr",
+    "host": "begnhhimrzdbwryxnjrr-mysql.services.clever-cloud.com",
     "dialect": "mysql",
     "dialectModule": require("mysql2"),
     "benchmark": true
 });
+
+// const sequelize = new Sequelize({
+//     "username": "root",
+//     "password": "",
+//     "database": "ttt",
+//     "host": "localhost",
+//     "dialect": "mysql",
+//     "dialectModule": require("mysql2"),
+//     "benchmark": true
+// });
 
 sequelize.authenticate()
     .then(() => {
