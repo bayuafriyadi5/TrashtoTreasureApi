150cd427e5cca7bf9f61a83f9f8899a33f64bd7a
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 23:04:51 +0700

    Update pembeliRoutes.js

--- routes/pembeliRoutes.js
+++ routes/pembeliRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const pembeliController = require('../controllers/pembeliController');
+const authenticateToken = require('../middleware/auth');
 
 router.get('/', pembeliController.getAllPembeli);
 router.get('/find', pembeliController.findPembeliByName);
 router.get('/:id_pembeli', pembeliController.getPembeliById);
 router.post('/register', pembeliController.registerPembeli);
 router.post('/login', pembeliController.loginPembeli);
-router.put('/', pembeliController.updatePembeli);
+router.put('/', authenticateToken, pembeliController.updatePembeli);
 router.delete('/', pembeliController.deletePembeli);
 
 module.exports = router;
