cf154e253319468645dec56b8066c8845c22ce8d
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 15:22:16 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,18 +10,15 @@ const authenticateToken = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const pembeli = await Pembeli.findByPk(decoded.id_pembeli);
+        const penjual = await Penjual.findByPk(decoded.id_penjual);
         if (!pembeli || pembeli.token !== token) {
             return res.status(401).json({ error: 'Invalid token' });
         }
         req.pembeli = pembeli;
-        next();
-
-        const penjual = await Penjual.findByPk(decoded.id_penjual);
-        if (!penjual || penjual.token !== token) {
-            return res.status(401).json({ error: 'Invalid token' });
-        }
         req.penjual = penjual;
         next();
+
+
     } catch (err) {
         res.status(400).json({ error: 'Invalid token' });
     }
