240d33eac000d4adb02e9710c8ee2fef53667df5
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 14:03:37 +0700

    Update produkRoutes.js

--- routes/produkRoutes.js
+++ routes/produkRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const produkController = require('../controllers/produkController');
-const authenticateToken = require('../middleware/authenticateToken'); // ensure you have this middleware correctly pathed
+const authenticateToken = require('../middleware/auth'); // ensure you have this middleware correctly pathed
 
 // Public routes
 router.get('/', produkController.getAllProduk);
@@ -9,7 +9,7 @@ router.get('/:id_produk', produkController.getProdukById);
 router.get('/find', produkController.findProdukByName);
 
 // Protected routes
-router.post('/', authenticateToken, produkWprodukController.createProdukteProduk);
+router.post('/', authenticateToken, produkController.createProdukteProduk);
 router.put('/', authenticateToken, produkController.updateProduk);
 router.delete('/', authenticateToken, produkController.deleteProduk);
 
