4f506d5e25fad6227e5bf43f0ea330cd3beeed29
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-30 04:35:09 +0700

    Update pembeliController.js

--- controllers/pembeliController.js
+++ controllers/pembeliController.js
@@ -101,7 +101,18 @@ exports.updatePembeli = async (req, res) => {
     try {
         const pembeli = req.pembeli;
         const { nama, email, telepon, alamat, password } = req.body;
-        const result = await Pembeli.update({ nama, email, telepon, alamat, password }, { where: { id_pembeli: pembeli.id_pembeli } });
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await Pembeli.update({
+            nama,
+            email,
+            telepon,
+            alamat,
+            password: hashedPassword // Use the hashed password
+        }, { where: { id_pembeli: pembeli.id_pembeli } });
+
         if (result[0]) {
             response(200, { isSuccess: result[0] }, "Successfully update data", res);
         } else {
