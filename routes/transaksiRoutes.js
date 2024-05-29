b54af2ce019888eb74148c2f73e9cd771681e253
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 19:33:45 +0700

    Update transaksiRoutes.js

--- routes/transaksiRoutes.js
+++ routes/transaksiRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const transaksiController = require('../controllers/transaksiController');
+const authenticateToken = require('../middleware/auth');
 
 router.get('/', transaksiController.getTransaksi);
 router.get('/:id_transaksi', transaksiController.getTransaksiById);
-router.post('/', transaksiController.createTransaksi);
+router.post('/', authenticateToken, transaksiController.createTransaksi);
 router.put('/', transaksiController.updateTransaksi);
-router.delete('/', transaksiController.deleteTransaksi);
+router.delete('/', authenticateToken, transaksiController.deleteTransaksi);
 
 module.exports = router;
