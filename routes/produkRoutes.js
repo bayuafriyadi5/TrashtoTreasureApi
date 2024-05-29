fd3c3004b53700622f54f4f65e1db331735775cc
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-05-29 14:13:42 +0700

    Update produkRoutes.js

--- routes/produkRoutes.js
+++ routes/produkRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const produkController = require('../controllers/produkController');
+const authenticateToken = require('../middleware/auth');
 
 router.get('/', produkController.getAllProduk);
 router.get('/:id_produk', produkController.getProdukById);
 router.get('/find', produkController.findProdukByName);
-router.post('/', produkController.createProduk);
-router.put('/', produkController.updateProduk);
-router.delete('/', produkController.deleteProduk);
+router.post('/', authenticateToken, produkController.createProduk);
+router.put('/', authenticateToken, produkController.updateProduk);
+router.delete('/', authenticateToken, produkController.deleteProduk);
 
 module.exports = router;
