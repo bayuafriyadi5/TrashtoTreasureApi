c22bc14037761dc56c4323e204a22178dd7cd0d6
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-08-04 14:42:21 +0700

    tambah midtrans

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
-
+const paymentMidtransController = require('../controllers/paymentMidtransController');
 
 router.post('/v2/invoices/', paymentController.createInvoice);
 router.get('/v2/invoices/:invoiceID', paymentController.getInvoice);
-
+router.get('/v1/invoices', paymentMidtransController.createInvoice);
 
 
 module.exports = router;
