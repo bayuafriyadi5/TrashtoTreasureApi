804278a55f7af4328f40901716c0c90139be0aaa
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-08-04 18:42:56 +0700

    delete

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const webhookRoutes = require('./routes/webhookRoutes');
 const midtransWebhookRoutes = require('./routes/midtransWebhookRoutes');
 const pembayaranRoutes = require('./routes/pembayaranRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
-const paymentWebhookRoutes = require('./routes/paymentWebhookRoutes'); // Check this line
 
 const port = 3000;
 
@@ -28,7 +27,7 @@ app.use('/webhook', webhookRoutes);
 app.use('/midtransWebhook', midtransWebhookRoutes);
 app.use('/pembayaran', pembayaranRoutes);
 app.use('/protected', protectedRoutes);
-app.use('/paymentWebhook', paymentWebhookRoutes); // Check this line
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
