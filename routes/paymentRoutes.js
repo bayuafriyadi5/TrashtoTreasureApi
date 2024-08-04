e25e409fb14e26e86b391c69085d16479ed10ace
bayu afriyadi <33390169+bayuafriyadi5@users.noreply.github.com>
2024-08-04 14:24:28 +0700

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
-const paymentMidtransController = require('../controllers/paymentMidtransController');
+// const paymentMidtransController = require('../controllers/paymentMidtransController');
 
 router.post('/v2/invoices/', paymentController.createInvoice);
 router.get('/v2/invoices/:invoiceID', paymentController.getInvoice);
